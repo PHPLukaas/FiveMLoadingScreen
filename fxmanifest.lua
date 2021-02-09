fc78ea48e9b67932821394ea54f8fe2a6317b375
Lukaas <35173609+PHPLukaas@users.noreply.github.com>
2021-02-09 21:09:05 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+-- Metadata de la ressource
+    fx_version 'cerulean'
+    games { 'common' }
+
+    author 'Lukas'
+    description 'Écran de chargement / Loading Screen pour FiveM developpé par Lukas'
+    version '1.0.0'
+
+-- Fichiers à importer
+
+    files {
+        'index.html',
+        'musiques/dua-lipa-levitating-feat-dababy-don-diablo-remix-official-audio.mp3',
+        'style.css',
+        'app.js',
+        'background.jpg'
+        'logo.png'
+    }
+
+-- Défini le loading Screen
+
+    loadscreen 'index.html'
